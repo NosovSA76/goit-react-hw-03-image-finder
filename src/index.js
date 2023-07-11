34c807d8c9a0e5a282720e4ba9bd7f8e2a65faac
NosovSA76 <125302593+NosovSA76@users.noreply.github.com>
2023-07-11 16:49:01 +0300

    deploy

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './components/theme';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
